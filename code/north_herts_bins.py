20c5f1547be0f55f84879bb88fd674e0c48aa23b
zoe chen <zoechensj@gmail.com>
2025-06-07 17:00:02 +0100

    update the scrapping code to reflect the website change

--- code/north_herts_bins.py
+++ code/north_herts_bins.py
@@ -3,14 +3,42 @@ import logging
 from dateutil import parser
 import re
 from typing import List, Tuple
-from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from contextlib import closing
 from os import getenv
 
-logger = logging.getLogger()
+
+# setup logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+# Regular expression to match the date format in the collection information
+DATE_PATTERN = re.compile(
+    r"(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)\s+(\d{1,2}(?:st|nd|rd|th)?\s+\w+\s+\d{4})"
+)
+
+
+def setup_chrome_driver() -> webdriver.Chrome:
+    """
+    Sets up the Chrome WebDriver with necessary options.
+    Returns a Chrome WebDriver instance.
+    """
+    # Set up Chrome options
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")  # optional, but useful in some cases
+    options.add_argument("--disable-software-rasterizer")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)  # Wait for elements to load
+    return driver
 
 
 def get_north_herts_bins() -> List[Tuple[datetime, str]]:
@@ -19,47 +47,88 @@ def get_north_herts_bins() -> List[Tuple[datetime, str]]:
     Recycling portal for upcoming bin collections and returns a List of
     Tuples (collection_date, collection_description)
     """
+    """ Returns the web address for my address bin collection page """
+    # load environment variables
     HOME_URL = getenv("HOME_URL")
-    if not HOME_URL:
+    POST_CODE = getenv("POST_CODE")
+    ADDRESS = getenv("ADDRESS")
+
+    if not HOME_URL or not POST_CODE or not ADDRESS:
         raise ValueError("HOME_URL environment variable is not set.")
     else:
-        date_pattern = re.compile(
-            r"(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)\s+(\d{1,2}(?:st|nd|rd|th)?\s+\w+\s+\d{4})"
+        logger.info(
+            "HOME_URL, POST_CODE and ADDRESS environment variables are set. Starting scraping..."
         )
-        options = Options()
-        options.add_argument("--headless")  # Use "--headless=new" if using a very new Chrome
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")  # optional, but useful in some cases
-        options.add_argument("--disable-software-rasterizer")
-        with closing(webdriver.Chrome(options=options)) as driver:
-            logger.info("Setting up Chrome driver for North Herts bins scraping...")
+
+        with closing(setup_chrome_driver()) as driver:
+            logger.info("Setting up Chrome driver for bins scraping...")
 
             # Set up the Chrome driver
-            driver.implicitly_wait(10)  # Wait for elements to load
+            wait = WebDriverWait(driver, 10)
             driver.get(HOME_URL)
-            time.sleep(3)  # Wait for JS to load — tweak this as needed
+            logger.info(f"started with {HOME_URL}")
 
-            soup = BeautifulSoup(driver.page_source, "html.parser")
-            raw_text = [
-                span.get_text()
-                for span in soup.find_all("span", class_="value-as-text")
-            ]
-            logger.info("Scraped raw text from North Herts bins page.")
+            # Input postcode
+            input_box = wait.until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "relation_path_type_ahead_search")
+                )
+            )
+            logger.info("find the input box for postcode and house number.")
+            # input the postcode in the input box, clear the input box first
+            input_box.clear()
+            input_box.send_keys(POST_CODE)
+            logger.info(f"input postcode: {POST_CODE}")
 
-        # find the date matches and their index in the raw_text
-        collection_dates = [
-            (i, date_pattern.search(item).group(0))
-            for i, item in enumerate(raw_text)
-            if date_pattern.search(item)
-        ]
-        parsed_dates = [(i, parser.parse(date_str)) for i, date_str in collection_dates]
-        # next_collection_date = min(date for _, date in parsed_dates)
+            # select the correct address from the dropdown
+            address_xpath = f'//li[contains(@aria-label, "{ADDRESS}")]'
+            try:
+                address_option = wait.until(
+                    EC.presence_of_element_located((By.XPATH, address_xpath))
+                )
+                address_option.click()
+                logger.info(f"input address options: {ADDRESS}")
 
-        # match bins to the bin date and only get the bins in the next collection date
-        matched_bins = [(date, raw_text[i - 1]) for i, date in parsed_dates]
+            except Exception as e:
+                logger.error(f"Address '{ADDRESS}' not found in the list.")
+                raise ValueError(
+                    f"Address {ADDRESS} isn't found in the dropdown. Please check the address."
+                ) from e
 
-        return matched_bins
+            # click the button to submit the address
+            button = wait.until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//input[@value='Select address and continue']")
+                )
+            )
+            button.click()
+            logger.info("Clicked the button to submit the address.")
+
+            """ Load to a new page with the collection information """
+            # find the collection information on the new page
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, "value-as-text")))
+            elements = driver.find_elements(By.CLASS_NAME, "value-as-text")
+
+            collection_info = [element.text for element in elements]
+            logger.info("Found collection information on the new page.")
+
+            matched_bins = []
+            for i, item in enumerate(collection_info):
+                match = DATE_PATTERN.search(item)
+                if match:
+                    try:
+                        collection_date = parser.parse(match.group(0))
+                        bin_name = collection_info[
+                            i - 1
+                        ]  # The bin name is usually the previous item
+                        matched_bins.append((collection_date, bin_name))
+                        logger.info(
+                            f"collection has found {collection_date} for {bin_name}"
+                        )
+                    except ValueError as e:
+                        logger.warning(f"Error parsing date from item '{item}': {e}")
+            logger.info(f"Find {len(matched_bins)} matched bins with collection dates.")
+            return matched_bins
 
 
 if __name__ == "__main__":
